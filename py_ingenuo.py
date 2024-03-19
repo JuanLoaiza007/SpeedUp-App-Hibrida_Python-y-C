ccdc9ad6472e03e2dec50a1885f2f9ddcf8dfd43
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-19 11:37:14 -0500

    implementation update

---
+++ py_ingenuo.py
@@ -0,0 +1,13 @@
+def main(arreglo, escalar):
+
+    resultado = []
+    for elemento in arreglo:
+        resultado.append(elemento * escalar)
+
+    print("Resultado: ", resultado)
+
+
+if __name__ == "__main__":
+    arreglo = [0.0, 1.0, 2.0, 3.0]
+    escalar = 2.0
+    main(arreglo, escalar)
